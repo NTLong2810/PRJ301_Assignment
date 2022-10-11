b1144ffdbf919615ac795c14f0bf7e0560834fff
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2022-10-11 14:38:14 +0700

    Create link url for groups.html
    Create link url (/group) for groups.html method ="GET"

--- MyProject/src/java/Controller/GroupsController.java
+++ MyProject/src/java/Controller/GroupsController.java
@@ -4,10 +4,25 @@
  */
 package Controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  *
  * @author DELL
  */
-public class GroupsController {
+public class GroupsController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("groups.html");
+    }
     
 }
