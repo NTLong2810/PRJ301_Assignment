14e27796ffd546ad70e5b3997ec2e86bed233e90
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2022-10-23 10:59:58 +0700

    Create URL patterns

--- MyProject/src/java/Controller/TimeTableController.java
+++ MyProject/src/java/Controller/TimeTableController.java
@@ -4,10 +4,25 @@
  */
 package Controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  *
  * @author DELL
  */
-public class TimeTableController {
+public class TimeTableController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
     
 }
