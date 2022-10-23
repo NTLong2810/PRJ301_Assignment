1c777820f7c5fb18df06b977ca069648c654e641
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2022-10-23 10:11:41 +0700

    Update Model

--- MyProject/src/java/model/Group.java
+++ MyProject/src/java/model/Group.java
@@ -4,10 +4,56 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author DELL
  */
 public class Group {
-    
+    private int id;
+    private String name;
+    private ArrayList<Student> students = new ArrayList<>();
+    private Subject subject;
+    private Lecturer supervisor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Lecturer getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Lecturer supervisor) {
+        this.supervisor = supervisor;
+    }
 }
